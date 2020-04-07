d34a47891740abe5c369c84dff13a67c3e590583
Kipwisp <61100140+Kipwisp@users.noreply.github.com>
2020-04-07 00:28:11 -0500

    Added custom text voice generation support for pony characters

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const bent = require('bent')
 const fs = require('fs') 
 const properties = require('./resources/properties.js')
 const config = require("./config.json");
+const characters = require("./resources/characters.json")
 
 const client = new Discord.Client();
 const post = bent(properties.api, 'POST', 'buffer', properties.headers);
@@ -11,15 +12,23 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
  });
 
-client.on('message', async msg => {
-    if (msg.content === "ping") {
-        let sentMessage = await msg.channel.send('Hold on this might take a bit...');
-        let data = {text:'Hello this is working!', character:'Twilight Sparkle'};
+client.on('message', async message => {
+    if (message.content[0] === config.prefix) {
+        let length = message.content.substring(config.prefix.length + 3).length;
+        if (length > config.char_limit) {
+            message.channel.send(`Your message is ${length - config.char_limit} characters over the character limit (${config.char_limit}).`);
+            return;
+        }
+
+        let sentMessage = await message.channel.send('Hold on this might take a bit...');
+        let character = message.content.substring(config.prefix.length, config.prefix.length + 2);
+        let line = message.content.substring(config.prefix.length + 3);
+        let data = {text:line, character:characters[character]};
         
         console.log("Processing request...");
         try {
             let response = await post('', data);
-            let file = 'output.wav';
+            let file = `${line}.wav`;
             
             fs.writeFile(file, response, (err) => { 
                 if (err) {
@@ -27,7 +36,7 @@ client.on('message', async msg => {
                 }
             }); 
 
-            await msg.channel.send({ files: [file] });
+            await message.channel.send({ files: [file] });
             sentMessage.delete();
             fs.unlinkSync(file, (err) => {
                 if (err) {
@@ -35,8 +44,8 @@ client.on('message', async msg => {
                 }
             });
         } catch (error) {
-            console.error;
-            msg.channel.send('Sorry, your request failed. Try again.');
+            console.log(error);
+            message.channel.send('Sorry, your request failed. Try again.');
             sentMessage.delete();
         }
         
