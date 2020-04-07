2ee4130494db9e6d35615f2ab34b1d98edff5014
Kipwisp <61100140+Kipwisp@users.noreply.github.com>
2020-04-07 18:28:03 -0500

    Updated headers

--- resources/properties.js
+++ resources/properties.js
@@ -1,17 +1,10 @@
 module.exports = {  
     headers: {
-        'authority': 'api.fifteen.ai',
-        'access-control-allow-origin': '*',
-        'accept': 'application/json, text/plain, */*',
-        'sec-fetch-dest': 'empty',
-        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Mobile Safari/537.36',
-        'dnt': '1',
-        'content-type': 'application/json;charset=UTF-8',
-        'origin': 'https://fifteen.ai',
-        'sec-fetch-site': 'same-site',
-        'sec-fetch-mode': 'cors',
-        'referer': 'https://fifteen.ai/app',
-        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        'Host': 'api.fifteen.ai',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:10.0) Gecko/20100101 Firefox/74.0',
+        'Access-Control-Allow-Origin': '*',
+        'Origin': 'https://fifteen.ai',
+        'Referer': 'https://fifteen.ai/app',
     },
     api: 'https://api.fifteen.ai/app/getAudioFile'
 };
