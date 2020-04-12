e427488d4d5e1b5b39891cc11aef1bad48802884
Kipwisp <61100140+Kipwisp@users.noreply.github.com>
2020-04-11 19:14:53 -0500

    Fixed bot permissions

--- commands/invite.js
+++ commands/invite.js
@@ -6,6 +6,6 @@ module.exports = {
     format: `${config.prefix}invite`,
     description: 'Sends the invite link for this bot.',
     exec: async (message) => { 
-        message.reply(`https://discordapp.com/api/oauth2/authorize?client_id=${config.client_id}&permissions=34816&scope=bot`);
+        message.reply(`https://discordapp.com/api/oauth2/authorize?client_id=${config.client_id}&permissions=3180544&scope=bot`);
     }
 };
