ee1b4448c3563ce801acfd5c7f1d85e509f85f6b
Kipwisp <61100140+Kipwisp@users.noreply.github.com>
2020-10-02 18:37:19 -0500

    Updated help message to display seperate pages

--- src/commands/help.js
+++ src/commands/help.js
@@ -3,13 +3,16 @@ const emotions = require('../../resources/emotions.json');
 const config = require('../../config.json');
 
 const name = 'Help';
+const charactersPerPage = 10;
+const maxPages = Math.ceil(Object.keys(characters).length / charactersPerPage);
 
 module.exports = {
     name,
-    command: new RegExp(`^${config.prefix}help$`),
-    format: `${config.prefix}help`,
-    description: 'Sends a help message containing commands for the bot and character codes.',
+    command: new RegExp(`^${config.prefix}help( [1-${maxPages}])?$`),
+    format: `${config.prefix}help n`,
+    description: 'Sends a help message containing commands for the bot and the n page for character codes.',
     exec: async (message) => {
+        const page = message.content.substr(config.prefix.length + 5, 1);
         let helpMessage = '```Powered by 15.ai\n\nCommands:\n';
 
         // eslint-disable-next-line global-require
@@ -20,8 +23,17 @@ module.exports = {
             }
         }
 
-        helpMessage += '\nCharacter codes and emotions:\n';
-        for (const character of Object.keys(characters)) {
+        if (page === '') {
+            helpMessage += `\nSay '${config.prefix}help n' to view nth (1-${maxPages}) page for characters and emotions.`;
+            message.channel.send(`${message.member} ${helpMessage}\`\`\``);
+            return;
+        }
+
+        helpMessage += `\nCharacter codes and emotions (Page ${page} of ${maxPages}):\n`;
+
+        const characterKeys = Object.keys(characters);
+        const selectedCharacters = characterKeys.slice((page - 1) * Math.floor(characterKeys.length / maxPages), page * Math.floor(characterKeys.length / maxPages));
+        for (const character of selectedCharacters) {
             helpMessage += `${character}: ${characters[character].name} \nEmotions: `;
 
             const characterEmotions = characters[character].emotions;
