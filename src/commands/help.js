23aefc0464fd3576fdb509674d347b3ae5d06d44
Kipwisp <61100140+Kipwisp@users.noreply.github.com>
2020-10-03 19:17:16 -0500

    Added timeout for help message

--- src/commands/help.js
+++ src/commands/help.js
@@ -9,7 +9,7 @@ module.exports = {
     name: 'Help',
     command: new RegExp(`^${config.prefix}help$`),
     format: `${config.prefix}help`,
-    description: 'Sends a help message containing commands for the bot and the pages for character codes.',
+    description: 'Sends a help message containing commands for the bot and the pages for character and emotion codes.',
     exec: async (message) => {
         const page = 1;
 
